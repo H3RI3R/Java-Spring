fa642d431d27993520805c13e6f51347853faef2
H3RI3R <ritiksoni101@gmail.com>
2023-03-24 17:11:51 +0530

    Initial commit

---
+++ src/test/java/com/example/RitikGitHub/RitikGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RitikGitHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RitikGitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
